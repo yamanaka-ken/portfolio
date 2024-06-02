0644bc25fad15dea39ea7096f29e03e389317f3d
yamanaka-ken <129924234+yamanaka-ken@users.noreply.github.com>
2024-06-03 00:37:47 +0900

    change article

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -27,25 +27,52 @@ function App() {
       <div className="container">
         <div className="left">
           <div className="theme">
-            <p>My hobbies</p>
+            <p>Ken Yamanaka</p>
           </div>
           <p className="medium-font">
-            cyclynig, running, snowbording, watching movies, cuisines
+          I am a first year graduate student and my major is electrical circuits.
+           I am exposed to programming with code and microcontrollers to analyze in experiments. 
+           In this portfolio, I will write about my skills and hobbies.
           </p>
-          <div className="theme">
-            <p>Age</p>
-          </div>
-          <p className="medium-font">23 years old</p>
-          <div className="theme">
-            <p>Majoring</p>
-          </div>
+          <div className="hr1"></div>
           <p className="medium-font">
-            Electronics(the diagnoisis of lithium-ion battery)
+            <strong>QUALUFICATION</strong>
+            <ul class="cp_list">
+            <li>Fundamental Information Technology Engineer Examination</li>
+            <li>TOEIC(L&R): Score 775</li>
+            <li>driver's license</li>
+            <li>Bachelor's degree</li>
+            </ul>
+            
           </p>
+          <p className="medium-font">
+            <strong>SKILLS</strong>
+
+            <div className="wrapper">
+              <div className="left">
+                  <ul class="cp_list">
+                  <li>Python</li>
+                  <li>C</li>
+                  <li>C++</li>
+                  <li>JavaScript</li>
+                  </ul>
+                </div>
+                <div className="right">
+                  <ul class="cp_list">
+                  <li>PSIM(Circuit-Simulater)</li>
+                  <li>LTSPice(Circuit-Simulater)</li>
+                  <li>MATLAB</li>
+                  <li>React</li>
+                  </ul>
+                </div>
+            </div>
+ 
+          </p>
+
         </div>
 
         <div className="right">
-          <p>I like to eat.</p>
+          <p>My hobbies..</p>
           <ImageSwitching />
         </div>
       </div>
@@ -87,11 +114,14 @@ function App() {
         </div>
         <div className="item">
           <p>Contact</p>
-          <p>E-mail address</p>
+
         </div>
       </div>
     </div>
   );
 }
 
+
+
 export default App;
+
